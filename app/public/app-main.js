4e529ee1c7c320850b6fbc57b7f428470c0a1351
v.borysov <v.borysov@limecs.net>
2024-10-11 13:50:59 +0200

    add webpack config

--- app/public/app-main.js
+++ app/public/app-main.js
@@ -1,103 +1,106 @@
-(() => { // webpackBootstrap
-"use strict";
-var __webpack_modules__ = ({
-"./app/src/index.scss?95eb": (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
-__webpack_require__.r(__webpack_exports__);
-// extracted by css-extract-rspack-plugin
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
 
+/***/ "./app/src/index.scss":
+/*!****************************!*\
+  !*** ./app/src/index.scss ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-}),
-"./app/src/js/test_imports.js": (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
-__webpack_require__.r(__webpack_exports__);
-__webpack_require__.d(__webpack_exports__, {
-  logjs: function() { return logjs; }
-});
-function logjs() {
-    console.log('js import works nice');
-}
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://constructor/./app/src/index.scss?");
 
+/***/ }),
 
+/***/ "./app/src/index.js":
+/*!**************************!*\
+  !*** ./app/src/index.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-}),
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _js_test_imports__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/test_imports */ \"./app/src/js/test_imports.js\");\n/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.scss */ \"./app/src/index.scss\");\n// run: npm run watch\n// save this file > check console\n// open index.scss\n\n\n(0,_js_test_imports__WEBPACK_IMPORTED_MODULE_0__.logjs)();\n\n\n//# sourceURL=webpack://constructor/./app/src/index.js?");
 
-});
+/***/ }),
+
+/***/ "./app/src/js/test_imports.js":
+/*!************************************!*\
+  !*** ./app/src/js/test_imports.js ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   logjs: () => (/* binding */ logjs)\n/* harmony export */ });\nfunction logjs() {\n    console.log('js import works nice');\n}\n\n\n\n//# sourceURL=webpack://constructor/./app/src/js/test_imports.js?");
+
+/***/ })
+
+/******/ 	});
 /************************************************************************/
-// The module cache
-var __webpack_module_cache__ = {};
-
-// The require function
-function __webpack_require__(moduleId) {
-
-// Check if module is in cache
-var cachedModule = __webpack_module_cache__[moduleId];
-if (cachedModule !== undefined) {
-return cachedModule.exports;
-}
-// Create a new module (and put it into the cache)
-var module = (__webpack_module_cache__[moduleId] = {
-exports: {}
-});
-// Execute the module function
-__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-
-// Return the exports of the module
-return module.exports;
-
-}
-
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
 /************************************************************************/
-// webpack/runtime/define_property_getters
-(() => {
-__webpack_require__.d = function(exports, definition) {
-	for(var key in definition) {
-        if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-            Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-        }
-    }
-};
-})();
-// webpack/runtime/has_own_property
-(() => {
-__webpack_require__.o = function (obj, prop) {
-	return Object.prototype.hasOwnProperty.call(obj, prop);
-};
-
-})();
-// webpack/runtime/make_namespace_object
-(() => {
-// define __esModule on exports
-__webpack_require__.r = function(exports) {
-	if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-		Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-	}
-	Object.defineProperty(exports, '__esModule', { value: true });
-};
-
-})();
-// webpack/runtime/rspack_version
-(() => {
-__webpack_require__.rv = function () {
-	return "1.0.8";
-};
-
-})();
-// webpack/runtime/rspack_unique_id
-(() => {
-__webpack_require__.ruid = "bundler=rspack@1.0.8";
-
-})();
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */var _js_test_imports__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/test_imports */ "./app/src/js/test_imports.js");
-/* harmony import */var _index_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.scss */ "./app/src/index.scss?95eb");
-// run: npm run watch
-// save this file > check console
-// open index.scss
-
-
-(0,_js_test_imports__WEBPACK_IMPORTED_MODULE_0__.logjs)();
-
-})()
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./app/src/index.js");
+/******/ 	
+/******/ })()
 ;
-//# sourceMappingURL=app-main.js.map
---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+// @ts-check
+
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const sass = require('sass');
+
+/** @type {import('webpack').Configuration} */
+let config = {
+  name: 'sass-watch-test',
+  mode: "development",
+  watchOptions: {
+    // ignored: /node_modules/,
+    // followSymlinks: true,
+    // aggregateTimeout: 200,
+    // poll: true,
+    // stdin: false,
+  },
+  // devtool: 'source-map',
+  cache: false,
+  // target: ['web', 'es5'],
+  entry: {
+    main: path.resolve('./app/src/index.js')
+  },
+  output: {
+    filename: 'app-[name].js',
+    path: path.resolve('./app/public'),
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'app-[name]-styles.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'swc-loader',
+        options: {
+          // sourceMaps: true,
+          // jsc: {
+          //   parser: {
+          //     syntax: "ecmascript",
+          //   },
+          //   target: "es5",
+          // },
+        },
+        exclude: [/node_modules/],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              // url: false,
+              // import: false,
+              // modules: false,
+              // sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: sass,
+              // sourceMap: true,
+              sassOptions: {
+                cache: false
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    // modules: ['node_modules', 'js_modules'],
+    // extensions: ['.js', '.css', '.scss']
+  },
+  experiments: {
+    // css: true,
+  },
+  optimization: {
+    // emitOnErrors: true,
+  },
+  externals: {
+    jquery: '$',
+  },
+  stats: {
+    // preset: 'detailed',
+    // errorDetails: true,
+    warnings: false
+  }
+}
+
+module.exports = config;
